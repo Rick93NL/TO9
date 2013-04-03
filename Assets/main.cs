adc7324d9b026ca757d01c4669cc75516446538d
Geerten <my.other.lifeHD@gmail.com>
2013-04-03 15:41:11 +0200

    Physics

--- Assets/main.cs
+++ Assets/main.cs
@@ -11,6 +11,6 @@ public class main : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate(Time.deltaTime*10, 0, 0);
+		transform.Translate(Time.deltaTime*5, 0, 0);
 	}
 }
