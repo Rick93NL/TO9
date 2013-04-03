4f76b04657fb58fbfbd2083f1c70adf6b4ffdabf
Geerten <my.other.lifeHD@gmail.com>
2013-04-03 10:16:06 +0200

    Sidescroller minigame initial commit

---
+++ Assets/main.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+	//private GameObject myCamera;
+	
+	// Use this for initialization
+	void Start () {
+		//myCamera = Camera.main.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Time.deltaTime*10, 0, 0);
+	}
+}
